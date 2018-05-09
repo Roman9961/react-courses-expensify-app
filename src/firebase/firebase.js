d816439312074cb8cb7cf32597e01cf635e03379
Roman <romanrimskiy@gmail.com>
2018-05-09 23:57:27 +0300

    Add env config firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyDy7gy4Ce_Qy8JXUsaN7r9TCqHN4g-_Aec",
-    authDomain: "expensify-c7e78.firebaseapp.com",
-    databaseURL: "https://expensify-c7e78.firebaseio.com",
-    projectId: "expensify-c7e78",
-    storageBucket: "expensify-c7e78.appspot.com",
-    messagingSenderId: "891010644755"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROGECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 firebase.initializeApp(config);
 const database = firebase.database();
